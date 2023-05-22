00380b378f98400e2563ee60824ba5690713146c
Nikos Portolos <nikos.portolos@gmail.com>
2023-05-22 03:24:35 +0300

    update coverage script

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -6,8 +6,8 @@ cd ..
 dart pub global run coverage:test_with_coverage || { echo 'test with coverage failed' ; exit 1; }
 
 # Remove auto-generated files from coverage
-lcov --remove coverage/lcov.info '*.gen.dart' -o coverage/ansix_lcov.info || { echo 'failed to exclude auto-generated files from coverage' ; exit 1; }
+lcov --remove coverage/lcov.info '*.gen.dart' -o coverage/trace_lcov.info || { echo 'failed to exclude auto-generated files from coverage' ; exit 1; }
 
 # Generate coverage report
-genhtml coverage/ansix_lcov.info -o coverage/html || { echo 'failed to generate coverage report' ; exit 1; }
+genhtml coverage/trace_lcov.info -o coverage/html || { echo 'failed to generate coverage report' ; exit 1; }
 
